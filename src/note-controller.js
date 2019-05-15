83f957cc258070d8b1e9738767f30e233f599960
domw30 <domw30@gmail.com>
2019-05-15 15:57:59 +0100

    Spike and then TDD code that shows a note in the index.html page

--- src/note-controller.js
+++ src/note-controller.js
@@ -1,6 +1,27 @@
-var element = document.getElementById("app");
+(function(exports) {
+  function NoteController(NoteList, NoteListView) {
+    this.noteList = new NoteList();
+    this.noteListView = new NoteListView(this.noteList);
+  };
 
-const content = element.innerHTML;
-element.innerHTML = "howdy man";
 
-console.log(content)
+NoteController.prototype.updateview = function(){
+    this.noteList.addNote('Favourite drink: seltzer');
+    var element = document.getElementById("app");
+    const content = element.innerHTML;
+    element.innerHTML = this.noteListView.getHTML();
+  };
+
+
+
+exports.NoteController = NoteController;
+})(this);
+
+
+var notecontroller = new NoteController(NoteList, NoteListView);
+
+notecontroller.updateview();
+
+// NoteList.prototype.addNote = function (noteText) {
+//   return this.list.push(new Note(noteText))
+// };
