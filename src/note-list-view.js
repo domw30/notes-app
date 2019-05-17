6fa55dd5e569c0cfc924a87927613c079b4ff21b
domw30 <domw30@gmail.com>
2019-05-17 16:58:40 +0100

    Use TDD to show only the first 20 characters of each note on the main note list page

--- src/note-list-view.js
+++ src/note-list-view.js
@@ -8,14 +8,21 @@
     var htmlString = '<ul>'
 
     for(var i = 0; i < this.noteList.length; i++) {
-        htmlString += '<li><div>' + this.noteList[i].displayText() + '</div></li>'
+        htmlString += '<li><div>' + reduceNote(this.noteList[i].displayText()) + '</div></li>'
     }
-
     htmlString += '</ul>';
-
     return htmlString
+
+    function reduceNote(text) {
+    var reducedNote = [];
+    var wholeNote = text;
+    for(var i = 0; i < 20; i++) {
+      reducedNote.push(wholeNote.charAt(i));
+    }
+    return reducedNote.join("");
   }
+}
+
 
   exports.NoteView = NoteView;
-
 })(this);
--- src/single-note-view.js
+++ src/single-note-view.js
@@ -5,5 +5,5 @@
   SingleNoteView.prototype.viewNote = function(note) {
     return `<div>${this.note.content}</div>`
   }
-  exports.SingleNoteView = SingleNoteView
+  exports.SingleNoteView = SingleNoteView;
 })(this);
